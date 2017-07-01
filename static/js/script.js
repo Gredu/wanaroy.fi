517c30ea27db11d941d8cb9c6667913fc148fb11
Greatman Lim <greatman.lim@mahti.org>
2017-07-01 15:36:50 +0300

    Init

---
+++ static/js/script.js
@@ -0,0 +1,30 @@
+function frontIn() {
+
+}
+
+function frontOut() {
+  // or just don't display the scroll?
+  Array.from(document.getElementsByClassName('hide-on-start')).forEach(function(e) {
+    e.style.display = 'block'
+  })
+  document.getElementsByClassName('front-display')[0].style.transform = 'translate(0, -100%)'
+}
+
+// main
+//
+let frontBackgrounds = [
+  'http://wanaroy.fi/images/gallery/08b.jpg',
+  'http://wanaroy.fi/images/gallery/07b.jpg',
+  'http://wanaroy.fi/images/gallery/06b.jpg',
+  'http://wanaroy.fi/images/gallery/02b.jpg'
+]
+
+// front
+Array.from(document.getElementsByClassName('front-a')).forEach(function(e, i) {
+  e.onclick  = function() {
+    console.log('link: ' + e.innerHTML)
+    console.log('index: ' + i)
+    console.log('put image: ' + frontBackgrounds[i])
+    document.getElementById('jumbotron').style.backgroundImage = 'url(' + frontBackgrounds[i] + ')'
+  }
+})
